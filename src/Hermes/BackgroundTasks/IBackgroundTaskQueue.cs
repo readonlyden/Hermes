6564dc5165dfd80847cd3e51efa167a43540dbd7
Denis Obolensky <denismaster@outlook.com>
2021-10-05 12:33:17 +0300

    Add basic BackgroundTaskQueue

---
+++ src/Hermes/BackgroundTasks/IBackgroundTaskQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hermes.BackgroundTasks
+{
+    public interface IBackgroundTaskQueue
+    {
+        ValueTask EnqueueAsync(Func<CancellationToken, ValueTask> workItem);
+
+        ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(
+            CancellationToken cancellationToken);
+    }
+}
---
+++ tests/Hermes.Tests/BackgroundTaskQueueTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hermes.BackgroundTasks;
+using Xunit;
+
+namespace Hermes.Tests
+{
+    public class BackgroundTaskQueueTests
+    {
+        private const int DefaultQueueCapacity = 10;
+
+        [Fact]
+        public async Task EnqueueDequeueShouldWorkAsExpected()
+        {
+            int numberOfCalls = 0;
+            CancellationToken cancellationToken = default;
+
+            Func<CancellationToken, ValueTask> func = _ =>
+            {
+                numberOfCalls++;
+                return ValueTask.CompletedTask;
+            };
+
+            var backgroundQueue = new BackgroundTaskQueue(DefaultQueueCapacity);
+
+            await backgroundQueue.EnqueueAsync(func);
+            var funcFromQueue = await backgroundQueue.DequeueAsync(cancellationToken);
+
+            await funcFromQueue(cancellationToken);
+
+            Assert.Equal(1, numberOfCalls);
+        }
+    }
+}
