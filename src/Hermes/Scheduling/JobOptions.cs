6a9b1d854b3226f1ef4a400b175e68ec24741412
Denis Obolensky <darthdenismaster@yandex.ru>
2022-01-03 23:40:56 +0300

    Add basic JobOptions

---
+++ src/Hermes/Scheduling/JobOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Hermes.Scheduling;
+
+public class JobOptions
+{
+    public string CronExpression { get; set; } = string.Empty;
+    public bool RunImmediately { get; set; } = false;
+    public string JobName { get; set; } = string.Empty;
+}
