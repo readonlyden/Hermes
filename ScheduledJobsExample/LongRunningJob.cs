c8a416b44db33c90c1149d92484634c7da4d4d15
Denis Obolensky <darthdenismaster@yandex.ru>
2022-01-07 14:55:55 +0300

    Add example of background jobs

---
+++ ScheduledJobsExample/LongRunningJob.cs
@@ -0,0 +1,20 @@
+﻿using Hermes.Scheduling;
+
+namespace ScheduledJobsExample;
+
+public class LongRunningJob : IJob
+{
+    private readonly ILogger<SimpleJob> _logger;
+
+    public LongRunningJob(ILogger<SimpleJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Long job started at: {time}", DateTimeOffset.Now);
+        await Task.Delay(5000);
+        _logger.LogInformation("Long job finished at: {time}", DateTimeOffset.Now);
+    }
+}
